01f04c1f952bbfa8ef0d2e62bd07e3f2971d6818
Caio de Souza <caiodesouzadev@hotmail.com>
2020-06-21 14:22:22 -0300

    first commit

---
+++ src/main/java/com/caiodesouza/hearthstoneapi/HearthstoneProject1Application.java
@@ -0,0 +1,13 @@
+package com.caiodesouza.hearthstoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthstoneProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearthstoneProject1Application.class, args);
+	}
+
+}
