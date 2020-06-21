916e9d2525a9928f28d6fdd3818636958a64e110
Caio de Souza <caiodesouzadev@hotmail.com>
2020-06-21 15:36:11 -0300

    Service and Search by ID

--- src/main/java/com/caiodesouza/hearthstoneapi/resources/CardResource.java
+++ src/main/java/com/caiodesouza/hearthstoneapi/resources/CardResource.java
@@ -1,22 +1,33 @@
 package com.caiodesouza.hearthstoneapi.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caiodesouza.hearthstoneapi.entities.Card;
-import com.caiodesouza.hearthstoneapi.entities.enums.CardClass;
-import com.caiodesouza.hearthstoneapi.entities.enums.CardType;
+import com.caiodesouza.hearthstoneapi.services.CardService;
 
 @RestController
 @RequestMapping(value= "/cards")
 public class CardResource {
 	
+	@Autowired
+	private CardService service;
+	
 	@GetMapping
-	public ResponseEntity<Card> findAll(){
-		Card c = new Card(1,"dragao","this card does something",7,9,CardType.Any,CardClass.Creature);
-		return ResponseEntity.ok().body(c);
+	public ResponseEntity<List<Card>> findAll(){
+		List<Card> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Card> findById(@PathVariable Integer id) {
+		Card obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+				}
 }
--- src/main/java/com/caiodesouza/hearthstoneapi/services/CardService.java
+++ src/main/java/com/caiodesouza/hearthstoneapi/services/CardService.java
@@ -1,5 +1,28 @@
 package com.caiodesouza.hearthstoneapi.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiodesouza.hearthstoneapi.entities.Card;
+import com.caiodesouza.hearthstoneapi.repositories.CardRepository;
+
+@Service
 public class CardService {
 
+	@Autowired
+	private CardRepository repository;
+	
+	public List<Card> findAll(){
+		return repository.findAll();
+	}
+	
+	public Card findById(Integer id) {
+		Optional<Card> obj = repository.findById(id);
+		return obj.get();
+	}
 }
+
+
